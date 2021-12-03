357f4b4cd1ee5401f9070aecdf68da174b5777d0
ArianneBonneau <arianne.bonneau@hotmail.com>
2021-12-03 11:26:45 -0500

    navigation bar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="fr">
+@extends('layouts.head')
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+{{--            <img class="navbar-brand" src="img/logo.png" alt="logo">--}}
+            <a class="navbar-brand" href="#">Moz-Art Polissage</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Services</a> </li>
+                <li><a href="#">Sousmissions</a></li>
+                <li><a href="#">Contactez-nous</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+    <div class="title">
+        <h1>Moz-Art Polissage</h1>
+        <p>Scroll for more!</p>
+    </div>
+
+
+
+</body>
